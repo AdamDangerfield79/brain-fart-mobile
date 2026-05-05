3f786f60f4992c6405fdc57e8acb306903c2d744
Adam Dangerfield <84838937+AdamDangerfield@users.noreply.github.com>
2026-05-05 16:23:33 +0100

    5

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,5 @@
-const CACHE_NAME="brain-fart-mobile-web-v0-04";const ASSETS=["./","./index.html","./styles.css","./app.js?v=004","./manifest.json","./icons/icon-192.svg","./icons/icon-512.svg"];self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)))});self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim()});self.addEventListener("fetch",e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))))
+const CACHE_NAME="brain-fart-mobile-web-v0-05";
+const ASSETS=["./","./index.html","./styles.css?v=005","./app.js?v=005","./manifest.json","./icons/icon-192.svg","./icons/icon-512.svg"];
+self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)))});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim()});
+self.addEventListener("fetch",e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
